0ee65d071c868c719ea64c3282655603a7cf0250
Kaden Scott <kscottdev@gmail.com>
2021-05-07 10:02:41 -0400

    colour updates

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,14 +1,41 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+	import ThemeToggle from '$lib/ThemeToggle/index.svelte'
 </script>
 
-<header>
-	<h1 class="title">$ kaden.sh</h1>
+<header class="header">
+	<div class="corner-left">
+
+	</div>
+	<div class="title">
+		<h1>$ kaden.sh</h1>
+	</div>
+	<div class="corner-right">
+		<ThemeToggle />
+	</div>
 </header>
 
 <style>
 	.title {
-		font-family: monospace;
+		font-family: var(--font-mono);
+		margin: 0;
+		padding: 0.1em 0.2em;
+		border-radius: 0.3em;
+		background-color: black;
+		color: #a8a8a8;
+	}
+
+	.title h1 {
+		padding: 0;
+		margin: 0;
+		height: fit-content;
+	}
+
+	.header {
+		background-color: var(--colour-bg-2);
+		transition: background-color 0.2s;
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		padding: 0.5em;
 	}
 </style>
