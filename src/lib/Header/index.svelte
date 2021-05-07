8294ffe57fef1b48f006a1cfcea5be5572e8b429
Kaden Scott <kscottdev@gmail.com>
2021-05-07 05:14:31 -0400

    initial commit

---
+++ src/lib/Header/index.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<h1 class="title">$ kaden.sh</h1>
+</header>
+
+<style>
+	.title {
+		font-family: monospace;
+	}
+</style>
