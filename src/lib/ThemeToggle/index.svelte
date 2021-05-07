918d09cdc861dd5ca387c3605313e6231569de0c
Kaden Scott <kscottdev@gmail.com>
2021-05-07 08:06:39 -0400

    add ThemeToggle component

---
+++ src/lib/ThemeToggle/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    $: text = "🌞"
+
+    function toggle() {
+        let present = window.document.documentElement.classList.toggle("light-mode")
+
+        text = present ? "🌞" : "🌝"
+    }
+</script>
+
+<button on:click={toggle}>
+    {text}
+</button>
+
+<style>
+    button {
+        background-color: var(--colour-bg-2);
+        color: white;
+        border-radius: 4px;
+        border: gray 1px solid;
+        font-size: 2em;
+    }
+</style>
