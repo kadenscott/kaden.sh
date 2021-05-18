500d7e571308b476beeb0c1a5de48e377aa0910d
Kaden Scott <kscottdev@gmail.com>
2021-05-18 09:55:10 -0400

    add social links to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,42 @@
 
 <script>
 	import MiniCard from '$lib/MiniCard/index.svelte'
+	import IconLink from '$lib/IconLink/index.svelte'
+
+	import {faDiscord, faYoutube, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
+	import {faFileCode} from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <svelte:head>
 	<title>$ kaden.sh</title>
 </svelte:head>
 
-<h1>Welcome to my website.</h1>
-<p>blah lbhal bhalhb lbha lah.</p>
+<h1>welcome to the website of <span class="gradient-green-blue gradient-text">kadenscott</span>.</h1>
+
+<ul class="social-links gradient-red-orange">
+	<li><IconLink icon={faDiscord} text="My Discord server" link="https://chat.ksc.sh"/></li>
+	<li><IconLink icon={faYoutube} text="YouTube" link="https://chat.ksc.sh"/></li>
+	<li><IconLink icon={faTwitter} text="Twitter" link="https://chat.ksc.sh"/></li>
+	<li><IconLink icon={faGithub} text="GitHub" link="https://chat.ksc.sh"/></li>
+	<li><IconLink icon={faFileCode} text="kscott.dev" link="https://kscott.dev"/></li>
+</ul>
+
+<style>
+	.social-links {
+		margin-top: 5em;
+		padding: 2em 0;
+		border-radius: 0.2em;
+		list-style-type: none;
+
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+
+	}
+
+	.social-links li {
+		font-size: 3rem;
+		margin: 0.1em;
+		display: inline;
+	}
+</style>
