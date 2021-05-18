d5cb643bbfd7fa7db831b415e0ef37078aa58b36
Kaden Scott <kscottdev@gmail.com>
2021-05-18 08:46:16 -0400

    update nav styling

--- src/lib/MainNav/index.svelte
+++ src/lib/MainNav/index.svelte
@@ -1,43 +1,39 @@
 <ul class="links">
-    <li class="link-element font-mono"><a href="projects"><h3>projects</h3></a></li>
+    <li class="link-element font-mono"><a href="/"><h3>~/</h3></a></li>
     <li class="link-sep font-mono">•</li>
-    <li class="link-element font-mono"><a href="blog"><h3>blog</h3></a></li>
+    <li class="link-element font-mono"><a href="/projects"><h3>~/projects</h3></a></li>
     <li class="link-sep font-mono">•</li>
-    <li class="link-element font-mono"><a href="about"><h3>about</h3></a></li>
+    <li class="link-element font-mono"><a href="/blog"><h3>~/blog</h3></a></li>
     <li class="link-sep font-mono">•</li>
-    <li class="link-element font-mono"><a href="contact"><h3>contact</h3></a></li>
+    <li class="link-element font-mono"><a href="/about"><h3>~/about</h3></a></li>
+    <li class="link-sep font-mono">•</li>
+    <li class="link-element font-mono"><a href="/contact"><h3>~/contact</h3></a></li>
 </ul>
 
 <style>
-    /*.links {*/
-    /*    list-style-type: none;*/
-    /*    padding: 0;*/
-    /*    display: flex;*/
-    /*    flex-direction: row;*/
-    /*    justify-content: center;*/
-    /*    align-items: center;*/
-    /*}*/
+    .links {
+        list-style-type: none;
+        padding: 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
 
-    /*.link-element {*/
-    /*    display: inline-block;*/
-    /*    background-color: var(--colour-dark-gray);*/
-    /*    margin: 0.2em;*/
-    /*    padding: 0.5em;*/
-    /*    border: 2px var(--colour-blue) solid;*/
-    /*    box-shadow: var(--colour-blue) 0 0 10px;*/
-    /*    border-radius: 0.25em;*/
+    .link-element {
+        display: inline-block;
+        border-radius: 0.25em;
+        padding-inline: 0.5em;
+        color: var(--theme-text-highlight);
+    }
 
-    /*    transition: all 0.2s ease-in-out;*/
-    /*}*/
+    .link-element:hover {
+        color: var(--theme-text-highlight-alt);
+    }
 
-    /*.link-element:hover {*/
-    /*    transform: scale(1.05);*/
-    /*    box-shadow: var(--colour-blue) 0 0 15px;*/
-    /*}*/
-
-    /*.link-element:active {*/
-    /*    background-color: var(--colour-dark-gray-2);*/
-    /*}*/
+    .link-element:active {
+        color: var(--theme-text);
+    }
 
     /*.link-sep {*/
     /*    font-size: 2em;*/
@@ -45,11 +41,14 @@
     /*    display: inline-block;*/
     /*}*/
 
-    /*.link-element a {*/
-    /*    color: var(--theme-text);*/
-    /*    text-decoration: none;*/
-    /*    display: inline-block;*/
-    /*}*/
+    .link-element a {
+        text-decoration: none;
+        display: inline-block;
+    }
+
+    .link-element a:visited, a:link {
+        color: inherit;
+    }
 
     /*h3 {*/
     /*    display: inline-block;*/
