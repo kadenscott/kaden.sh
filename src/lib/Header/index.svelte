77c44e1041b1545c53c83553ee7584361c7fdd2d
Kaden Scott <kscottdev@gmail.com>
2021-05-18 08:45:59 -0400

    improve header

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,8 +1,6 @@
 <script>
     import {page} from "$app/stores"
 
-    import Splash from '$lib/Splash/index.svelte'
-    import ThemeToggle from '$lib/ThemeToggle/index.svelte'
     import MainNav from '$lib/MainNav/index.svelte'
 
     let isExpanded
@@ -13,25 +11,45 @@
 </script>
 
 <header class="header" class:header-expanded={isExpanded}>
-    <h2 class="header__title">$ kaden.sh</h2>
-    <nav class="header__nav">
-        <h3 class="link"><a href="/">~/</a></h3>
-        <h3 class="link"><a href="/projects">~/projects</a></h3>
-        <h3 class="link"><a href="/blog">~/blog</a></h3>
-        <h3 class="link"><a href="/about">~/about</a></h3>
-    </nav>
+    <div class="header__nav">
+        <h2 class="header__title">$ kaden.sh</h2>
+        <MainNav class="header__nav"/>
+    </div>
+    <div class="header__banner">
+        <h2>This site is under construction.</h2>
+    </div>
 </header>
 
 <style>
     .header {
-        margin-top: 1em;
         align-self: center;
         width: 70vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin-bottom: 2.5em;
+
+        background-color: var(--theme-bg-alt);
+    }
+
+    .header__nav {
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
-        margin-bottom: 2.5em;
+
+        flex: 1;
+    }
+
+    .header__banner {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        flex: 1;
+
+        background-color: var(--colour-red-dark);
     }
 
     .link a {
@@ -39,13 +57,4 @@
         color: inherit;
     }
 
-    .link--current {
-        transform: scale(1.1);
-    }
-
-    .header__nav {
-        display: flex;
-        flex-direction: row;
-
-    }
 </style>
