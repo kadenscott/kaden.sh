66572c8b826c1ba936569b6a2899ab5f67782e9c
Kaden Scott <kscottdev@gmail.com>
2021-05-18 09:52:49 -0400

    change width settings

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -18,8 +18,8 @@
         align-items: center;
         justify-content: flex-start;
 
-        width: 100vw;
-        height: 100vh;
+        min-width: 100vw;
+        min-height: 100vh;
 
         background-color: var(--theme-bg);
         color: var(--theme-text);
