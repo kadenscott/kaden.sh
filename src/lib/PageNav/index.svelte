59708229dd1be930a1d96ac97527e128a0d94789
Kaden Scott <kscottdev@gmail.com>
2021-06-14 09:08:21 -0400

    add PageNav

---
+++ src/lib/PageNav/index.svelte
@@ -0,0 +1,26 @@
+<style>
+    .nav {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        list-style-type: none;
+        padding: 0;
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    .nav__link > a {
+        background-color: black;
+        color: white;
+        padding: 0.35em;
+        text-decoration: none;
+        border-radius: 0.25em;
+    }
+</style>
+
+<ul class="nav">
+    <li class="nav__link font-mono"><a href="/">index</a></li>
+    <li class="nav__link font-mono"><a href="/">projects</a></li>
+    <li class="nav__link font-mono"><a href="/">blog</a></li>
+</ul>
