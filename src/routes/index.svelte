dd771977565e2f16474bc362387af6a167e11e92
Kaden Scott <kscottdev@gmail.com>
2021-06-14 08:32:23 -0400

    add routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,31 @@
 <script context="module">
-	import ProjectList from "$lib/ProjectList/index.svelte";
+    import ProjectList from "$lib/ProjectList/index.svelte";
 
-	export const prerender = true;
+    export const prerender = true;
+
+    export const load = async ({fetch}) => {
+        const res = await fetch('/api/projects.json')
+
+        if (res.ok) {
+            let projects = await res.json();
+
+            return {
+                props: {projects}
+            }
+        } else {
+            return {
+                props: {projects: []}
+            }
+        }
+    }
+</script>
+
+<script>
+    export let projects;
 </script>
 
 <svelte:head>
-	<title>kaden.sh</title>
+    <title>kaden.sh</title>
 </svelte:head>
 
-<ProjectList />
+<ProjectList projects={projects}/>
