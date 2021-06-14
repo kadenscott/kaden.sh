b5c56d0b3144bfdc21c6f7196390454bf61bd783
Kaden Scott <kscottdev@gmail.com>
2021-06-14 07:29:07 -0400

    move project list stuff to separate component

---
+++ src/lib/ProjectList/index.svelte
@@ -0,0 +1,36 @@
+<script>
+    import ProjectCard from "$lib/ProjectCard/index.svelte";
+</script>
+
+<h1>Projects</h1>
+<div class="projects">
+    <ProjectCard
+            name="mc.ksc.sh"
+            description="My Minecraft server, filled with tons of fun stuff."
+            hasLink="true"
+            link="https://mc.ksc.sh"
+            linkText="IP: mc.ksc.sh"
+    />
+    <ProjectCard
+            name="Quantum"
+            description="Minecraft plugin for random teleportation."
+            hasLink="true"
+            link="https://github.com/kadenscott/Quantum"
+            linkText="GitHub.com"
+    />
+    <ProjectCard
+            name="BingBonk"
+            description="A fast-paced Minecraft minigame."
+            hasLink="true"
+            link="https://github.com/kadenscott/BingBonk"
+            linkText="GitHub.com"
+    />
+</div>
+
+
+<style>
+    .projects {
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,11 @@
 <script context="module">
-	import ProjectCard from "$lib/ProjectCard/index.svelte";
+	import ProjectList from "$lib/ProjectList/index.svelte";
 
 	export const prerender = true;
-</script>
-
-<script>
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>Projects</h1>
-	<div class="projects">
-		<ProjectCard
-				name="mc.ksc.sh"
-				description="My Minecraft server, filled with tons of fun stuff."
-				hasLink="true"
-				link="https://mc.ksc.sh"
-				linkText="Check it out!"
-		/>
-		<ProjectCard
-				name="Quantum"
-				description="Minecraft plugin for random teleportation."
-				hasLink="true"
-				link="https://github.com/kadenscott/Quantum"
-				linkText="IP: mc.ksc.sh"
-		/>
-		<ProjectCard
-				name="BingBonk"
-				description="A fast-paced Minecraft minigame."
-				hasLink="true"
-				link="https://github.com/kadenscott/BingBonk"
-				linkText="GitHub.com"
-		/>
-	</div>
-</section>
-
-<style>
-	.projects {
-		margin-top: 1em;
-		margin-bottom: 1em;
-	}
-</style>
+<ProjectList />
