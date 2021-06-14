c04ee7a98acb458a3896ade4d01efdc2b82333db
Kaden Scott <kscottdev@gmail.com>
2021-06-14 08:41:15 -0400

    add some secret stuff to ProjectList

--- src/lib/ProjectList/index.svelte
+++ src/lib/ProjectList/index.svelte
@@ -1,13 +1,18 @@
 <script>
-    // Project loading is in index.svelte route, as regular components cannot have a context="module" script tag.
-
     import ProjectCard from '$lib/ProjectCard/index.svelte'
     export let projects = [];
 </script>
 
-<h1>Projects</h1>
-<div class="projects">
-    {#each projects as project (project.name)}
+<div class="projects__container">
+    <div class="projects__title">
+        <h1 class="projects__title__text">Projects</h1>
+<!--        <form class="projects__search">-->
+<!--            <input type="text" class="projects__search__input">-->
+<!--            <input type="submit" value="Search">-->
+<!--        </form>-->
+    </div>
+    <div class="projects">
+        {#each projects as project (project.name)}
             <ProjectCard
                     name="{project.name}"
                     description="{project.description}"
@@ -15,13 +20,31 @@
                     link="{project.link}"
                     linkText="{project.linkText}"
             />
-    {/each}
+        {/each}
+    </div>
 </div>
-
 
 <style>
     .projects {
         margin-top: 1em;
         margin-bottom: 1em;
+    }
+
+    .projects__title {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .projects__search {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .projects__search__input {
+        font-size: 1.25rem;
     }
 </style>
