21acb35ec2ed67f82997249f02faa83b3c78ac3f
Kaden Scott <kscottdev@gmail.com>
2021-06-14 07:18:08 -0400

    make layout flex

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,13 +11,20 @@
     <div class="layout__content">
         <Header/>
 
-        <hr>
+        <div class="spacer">
+            <hr>
+        </div>
 
         <main>
             <slot/>
         </main>
 
+        <div class="spacer">
+            <hr>
+        </div>
+
         <footer>
+            <p>with 😡 and ☕ from 🇨🇦 </p>
         </footer>
     </div>
 </div>
@@ -25,11 +32,23 @@
 <style>
     .layout__content {
         width: 66ch;
+        display: flex;
+        flex-direction: column;
     }
 
     .layout {
         display: flex;
         flex-direction: row;
         justify-content: center;
+        height: 100vh;
+    }
+
+    .spacer {
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    .spacer hr {
+        flex-grow: 1;
     }
 </style>
