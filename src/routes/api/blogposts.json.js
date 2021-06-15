4d9d4f2aa10714b9ebb155f52f703f4ee2368368
Kaden Scott <kscottdev@gmail.com>
2021-06-15 19:22:45 -0400

    add blogposts.json route

---
+++ src/routes/api/blogposts.json.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import fs from 'fs'
+
+const posts = []
+
+const getAllPosts = () => {
+    try {
+        return fs.readdirSync("../static/posts").map((fileName) => {
+            const post = fs.readFileSync(
+                path.resolve("../static/posts", fileName),
+                "utf-8"
+            );
+            return grayMatter(post).data;
+        });
+    } catch (e) {
+        return [];
+    }
+};
+
+path.join()
+
+
+/**
+ * Returns a list of all blog posts.
+ * @param req request
+ * @return response
+ */
+export const get = async (req) => {
+    return {body: projects};
+}
