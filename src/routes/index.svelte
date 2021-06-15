7953d1c0ccc318cdb661ee6ddcc808a004e00440
Kaden Scott <kscottdev@gmail.com>
2021-06-15 01:03:58 -0400

    improve index.svelte random project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,25 @@
 <script context="module">
-    export const prerender = true;
-
     import ProjectCard from "$lib/ProjectCard/index.svelte";
-    import PageNav from "$lib/PageNav/index.svelte";
 
-    export const load = async ({fetch}) => {
+    export async function load({fetch}) {
+        let project = null
+
         const res = await fetch('/api/projects.json')
 
         if (res.ok) {
             let projects = await res.json();
 
-            return {
-                props: {projects}
-            }
-        } else {
-            return {
-                props: {projects: []}
-            }
+            project = projects[Math.floor(Math.random() * projects.length)]
+        }
+
+        return {
+            props: {project: project}
         }
     }
 </script>
 
 <script>
-    export let projects = [];
-
-    let project = projects[Math.floor(Math.random() * projects.length)]
+    export let project
 </script>
 
 <svelte:head>
@@ -35,7 +30,7 @@
     <p>Hi, welcome to my website. This place hosts all sorts of stuff. Click around to explore.</p>
 
     <ProjectCard
-            name="{project.name}"
+            name={project.name}
             description="{project.description}"
             hasLink="{project.hasLink}"
             link="{project.link}"
